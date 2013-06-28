e15eab1bf400082a3c39ee28adf1681803f9966e
Immobilizer <karl.udris@gmail.com>
2013-06-27 21:25:25 -0400

    Added Express server

---
+++ concepts/illum_jun23/app.js
@@ -0,0 +1,74 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express')
+  , routes = require('./routes')
+  , user = require('./routes/user')
+  , http = require('http')
+  , path = require('path')
+  //, wsock = require('socket.io').listen(http)
+  , tcpsock = require('net');
+
+var app = express();
+var server = app.listen(8888);
+var io = require('socket.io').listen(server);
+
+var tcp_HOST = 'localhost';
+var tcp_PORT = 4040;
+
+// all environments
+//app.set('port', process.env.PORT || 8888);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+app.get('/', routes.index);
+app.get('/users', user.list);
+
+//http.createServer(app).listen(app.get('port'), function(){
+//  console.log('Express server listening on port ' + app.get('port'));
+//});
+
+io.sockets.on('connection', function (socket) { 
+
+	var tcpClient = new tcpsock.Socket();
+	tcpClient.setEncoding("ascii");
+	tcpClient.setKeepAlive(true);
+
+	tcpClient.connect(tcp_PORT, tcp_HOST, function() {
+		console.info('CONNECTED TO : ' + tcp_HOST + ':' + tcp_PORT);
+
+		/*
+		 * Recieves data from TCP and sends it to browser.
+		 */
+		tcpClient.on('data', function(data) {
+			console.log('DATA: ' + data);
+			socket.emit("httpServer", data);
+		});
+
+		tcpClient.on('end', function(data) {
+			console.log('END DATA : ' + data);
+		});
+
+	});
+
+	// Receives data from the browser
+	socket.on('tcp', function(message) {
+		console.log('"tcp" : ' + message);
+		tcpClient.write(message);
+		return;
+	});
+
+});
